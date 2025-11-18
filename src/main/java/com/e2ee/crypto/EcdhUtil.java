7e84009f1baee97b16bfc92f04c9cbfdb1b816a0
minchey <nolam@naver.com>
2025-11-19 00:09:12 +0900

    hkdf-Extract 작성

--- src/main/java/com/e2ee/crypto/EcdhUtil.java
+++ src/main/java/com/e2ee/crypto/EcdhUtil.java
@@ -1,6 +1,9 @@
 package com.e2ee.crypto;
 
 import javax.crypto.KeyAgreement;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
@@ -9,7 +12,7 @@ import java.security.PublicKey;
 public class EcdhUtil {
 
     // 1) X25519 키쌍 생성
-    public static KeyPair generateKeyPair() throws Exception{
+    public static KeyPair generateKeyPair() throws Exception {
 
         // 1. X25519 키쌍을 생성할 수 있는 KeyPairGenerator 공장 불러오기
         KeyPairGenerator generator = KeyPairGenerator.getInstance("X25519");
@@ -24,7 +27,7 @@ public class EcdhUtil {
     }
 
     // 2) 내 개인키 + 상대 공개키로 공유 비밀키 생성하기
-    public static byte[] deriveSharedSecret(PrivateKey myPrivate, PublicKey theirPublic) throws Exception{
+    public static byte[] deriveSharedSecret(PrivateKey myPrivate, PublicKey theirPublic) throws Exception {
 
         //1. X25519용 비밀 공유 기계(KeyAgreement) 준비
         KeyAgreement ka = KeyAgreement.getInstance("X25519");
@@ -41,4 +44,27 @@ public class EcdhUtil {
         return sharedSecret;
     }
 
+    // 3) 공유 비밀키에서 AES 키 뽑아내기 (HKDF-SHA256, 정석)
+    public static SecretKey deriveAesKeyFromSharedSecret(byte[] sharedSecret) throws Exception {
+
+    }
+
+    private static byte[] hkdfExtract(byte[] salt, byte[] ikm) throws Exception {
+        //1. salt가 없으면 32바이트 0으로 대체
+        if(salt == null || salt.length ==0){
+            salt = new byte[32]; //자동으로 0으로 채워짐
+        }
+
+        // 2. HmacSHA256 준비 (키 = salt)
+        Mac mac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec keySpec = new SecretKeySpec(salt, "HmacSHA256");
+        mac.init(keySpec);
+
+        // 3. ikm(sharedSecret)을 넣어서 PRK 생성
+        return mac.doFinal(ikm);
+    }
+
+    private static byte[] hkdfExpand(byte[] prk, byte[] info, int length) throws Exception {
+
+    }
 }
